cb4e71b90fba22ca169e41dc45e8be1d2deddf4a
박석원 <rty3394@naver.com>
2020-05-28 03:56:13 +0900

    Update Omok.java

--- src/Socket Omok/Omok.java
+++ src/Socket Omok/Omok.java
@@ -490,7 +490,7 @@ class OmokState {
 		if(caseSum>1) {
 			return 1;
 		}
-		if(moveResultWin(stepCount,skip) == 0) {
+		if(moveResultWin(stepCount) == 0) {
 			winner = currentPlayer;
 		}
 		return 3;
@@ -631,12 +631,12 @@ class OmokState {
 			return 0;
 		return 3;
 	}*///원본코드
-	public int moveResultWin(int[] stepCount, boolean skip[]) {	// return값이 1,2면 false, 0이면 승자 결정.
+	public int moveResultWin(int[] stepCount) {	// return값이 1,2면 false, 0이면 승자 결정.
 
 		boolean win = false;
 		for (int i=0; i<8; i++) {
 				if (i % 2 == 1 && (stepCount[i-1] + stepCount[i] == 5-1))
-					if(skip[i] == false && skip[i-1] == false)			// 북 + 남 = 5, 동 + 서 = 5, 
+							// 북 + 남 = 5, 동 + 서 = 5, 
 					win = true;
 		}
 		
